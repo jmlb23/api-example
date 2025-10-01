698061b3bfe0044fe6077b3f9e2185289600565f
jmlb23 <rmmenosrf@riseup.net>
2025-10-01 18:13:18 +0200

    add dependencies and remove duplicated generated code

--- src/Program.cs
+++ src/Program.cs
@@ -2,16 +2,33 @@ using api.Features.Users.Domain.Query;
 using api.Features.Users.Infra;
 using api.Features.Users.Handlers;
 
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore;
-using Microsoft.Extensions.DependencyInjection; 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddScoped<IGetUserByIdQuery, GetUserByIdQuery>();
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+
+}
 
 
 var todosApi = app.MapGroup("/users");
