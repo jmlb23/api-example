a0a835e3344687b7efd9eaf2dbc133feaec2b03b
jmlb23 <rmmenosrf@riseup.net>
2025-10-02 12:53:52 +0200

    add csproj

--- Program.cs
+++ Program.cs
@@ -11,20 +11,21 @@ using System;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IGetUserByIdQuery, GetUserByIdQuery>();
 
-builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument();
+    
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseOpenApi();
+    app.UseSwaggerUi(); 
 }
 
-
-var todosApi = app.MapGroup("/users");
-todosApi.MapGet("/{id}", async (Guid id, IGetUserByIdQuery query) => {
+var usersApi = app.MapGroup("/users");
+usersApi.MapGet("/{id}", async (Guid id, IGetUserByIdQuery query) => {
     var result = await ListUsers.Handle(new ListUsers.Request(id), query);
     return Results.Ok(result);
 });
 
-app.Run();
+app.Run("http://localhost:5288");
