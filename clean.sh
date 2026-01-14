48b51e88eb12478b06fb0a4b9df705876c659b33
jmlb23 <rmmenosrf@riseup.net>
2026-01-14 12:58:55 +0100

    add core to cleanup

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bazelisk clean --expunge ; dotnet clean api.csproj ; dotnet clean Features/features.csproj ; dotnet clean Features/Auth/auth.csproj ; dotnet clean Features/Users/users.csproj ; dotnet clean Features/Publications/publications.csproj
+bazelisk clean --expunge ; dotnet clean api.csproj ; dotnet clean Features/Core/core.csproj ; dotnet clean Features/features.csproj ; dotnet clean Features/Auth/auth.csproj ; dotnet clean Features/Users/users.csproj ; dotnet clean Features/Publications/publications.csproj
