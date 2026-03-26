bb873e955a3f5c64c195a6bdea415c8e10a501f3
jmlb23 <rmmenosrf@riseup.net>
2026-03-26 12:18:41 +0100

    Name generic to a more intuitive name

--- Features/Core/Cqrs.cs
+++ Features/Core/Cqrs.cs
@@ -3,13 +3,13 @@ using System.Threading.Tasks;
 
 namespace api.Features;
 
-public sealed interface IOperation;
+public interface IOperation;
 
 public interface Command : IOperation;
 
 public interface Query<T> : IOperation;
 
-public interface IHandler<in TCommand, TResult> where TCommand : IOperation
+public interface IHandler<in TOperation, TResult> where TOperation : IOperation
 {
-    Task<TResult> Handle(TCommand command);
+    Task<TResult> Handle(TOperation command);
 }
